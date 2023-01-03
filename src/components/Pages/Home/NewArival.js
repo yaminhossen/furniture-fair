396eb2b6e87acafc68463488f6a51eb9801897e5
yaminhossen <streatdot616@gmail.com>
2023-01-03 15:07:53 +0600

    firebase ins

--- src/components/Pages/Home/NewArival.js
+++ src/components/Pages/Home/NewArival.js
@@ -15,7 +15,7 @@ import f23 from '../../../images/furniture/furniture (23).jpg';
 const NewArival = () => {
     return (
         <div className='w-3/4 mx-auto my-12'>
-            <h2 className='font-bold text-3xl font-bold font-serif my-3 text-stone-500 title  '>New arival</h2>
+            <h2 className='text-3xl font-bold font-serif my-3 text-stone-500 title  '>New arival</h2>
             <div className=' '>
                 <div className='flex w-full gap-4 '>
 
@@ -23,15 +23,11 @@ const NewArival = () => {
 
                     <div className='w-1/3'>
                         <div className='w-full  '><img className='image1 w-full' src={f17} alt="" /></div>
-                        {/* <div className=' flex gap-1 mt-1 w-full'>
-                            <div className='w-full'><img src={f2} className='w-full' alt="" /></div>
-                            <div className='w-full'><img src={f1} className='w-full' alt="" /></div>
-                        </div> */}
+
                     </div>
                     <div className='w-1/3  '>
                         <div className='w-full gap-4  '><img className='image2 w-full' src={f6} alt="" /></div>
                         <div className=' flex gap- mt-4 w-full'>
-                            {/* <div className='w-full'><img src={f5} className='w-full' alt="" /></div> */}
                             <div className='w-full'><img src={f26} className='w-full image2' alt="" /></div>
                         </div>
                     </div>
--- src/components/Pages/Shared/Header.js
+++ src/components/Pages/Shared/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../../images/logo/cameralogo.png';
 import './Header.css';
@@ -14,10 +13,11 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/'>Item 1</Link></li>
-                            <li><Link to='/'>Item 1</Link></li>
-                            <li><Link to='/'>Item 1</Link></li>
-                            <li><Link to='/'>Login</Link></li>
+                            <li><Link to='/home'>Home</Link></li>
+                            <li><Link to='/blog'>Blog</Link></li>
+                            <li><Link to='/contuct'>ContuctUs</Link></li>
+
+                            <li><Link to='/login'>Login</Link></li>
 
                         </ul>
                     </div>
